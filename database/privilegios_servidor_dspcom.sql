f862da3ab0d420bf5567d787cbffe2b2635c774b
Carlos Pinto <pintocar83@gmail.com>
2022-05-19 17:22:43 -0400

    SQL Previlegios sobre las tablas para el usuario dspcom en el servidor

---
+++ database/privilegios_servidor_dspcom.sql
@@ -0,0 +1,39 @@
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA modulo_base TO dspcom_siga;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA modulo_base TO dspcom_siga;
+GRANT ALL ON SCHEMA modulo_base TO dspcom_siga;
+GRANT USAGE ON SCHEMA modulo_base TO dspcom_siga;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA modulo_asl TO dspcom_siga;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA modulo_asl TO dspcom_siga;
+GRANT ALL ON SCHEMA modulo_asl TO dspcom_siga;
+GRANT USAGE ON SCHEMA modulo_asl TO dspcom_siga;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA modulo_asistencia TO dspcom_siga;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA modulo_asistencia TO dspcom_siga;
+GRANT ALL ON SCHEMA modulo_asistencia TO dspcom_siga;
+GRANT USAGE ON SCHEMA modulo_asistencia TO dspcom_siga;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA modulo_inventario TO dspcom_siga;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA modulo_inventario TO dspcom_siga;
+GRANT ALL ON SCHEMA modulo_inventario TO dspcom_siga;
+GRANT USAGE ON SCHEMA modulo_inventario TO dspcom_siga;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA modulo_nomina TO dspcom_siga;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA modulo_nomina TO dspcom_siga;
+GRANT ALL ON SCHEMA modulo_nomina TO dspcom_siga;
+GRANT USAGE ON SCHEMA modulo_nomina TO dspcom_siga;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA modulo_planificacion TO dspcom_siga;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA modulo_planificacion TO dspcom_siga;
+GRANT ALL ON SCHEMA modulo_planificacion TO dspcom_siga;
+GRANT USAGE ON SCHEMA modulo_planificacion TO dspcom_siga;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA modulo_planificador TO dspcom_siga;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA modulo_planificador TO dspcom_siga;
+GRANT ALL ON SCHEMA modulo_planificador TO dspcom_siga;
+GRANT USAGE ON SCHEMA modulo_planificador TO dspcom_siga;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO dspcom_siga;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO dspcom_siga;
+GRANT ALL ON SCHEMA public TO dspcom_siga;
+GRANT USAGE ON SCHEMA public TO dspcom_siga;
