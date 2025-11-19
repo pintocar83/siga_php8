7071d35f383339d9e5d15502e2763f03fd4c2d40
Carlos Pinto <pintocar83@gmail.com>
2025-11-18 20:47:24 -0400

    Correccion formato constancia de trabajo para periodos semanales

--- report/ficha_constancia_trabajo_alcaldia_mejia.php
+++ report/ficha_constancia_trabajo_alcaldia_mejia.php
@@ -6,34 +6,18 @@ include_once("../library/db.controller.php");
 include_once("../library/siga.config.php");
 include_once("../library/siga.class.php");
 
-// Si existe un reporte personalizado para la institucion abrirlo en vez del default
-// Por Ejemplo: Base de datos "alcaldia_mejia" segun key en siga.config.php
-// Forma 1: report/orden_pago_alcaldia_mejia.php
-// Forma 2: report/alcaldia_mejia/orden_pago.php
-$database = SIGA::database();
-$report="ficha_constancia_trabajo_$database.php";
-if(file_exists($report)){
-    header("Location: $report?".$_SERVER['QUERY_STRING']);
-}
 
-$report="$database/ficha_constancia_trabajo.php";
-if(file_exists($report)){
-    header("Location: $report?".$_SERVER['QUERY_STRING']);
-}
-
-
-include_once("../library/fpdf/1.84/WriteTag.php");
+include_once("../library/fpdf/1.7/WriteTag.php");
 
 include_once("../library/functions/letra_numero.php");
 include_once("../library/functions/letra_mes.php");
+include_once("../library/functions/formatDate.php");
 
 include_once("../class/nomina.class.php");
 include_once("../class/ficha.class.php");
 
-
 $nacionalidad=SIGA::paramGet("nacionalidad");
 $cedula=SIGA::paramGet("cedula");
-
 
 
 if($nacionalidad and $cedula){
@@ -70,20 +54,12 @@ else{
 $detalle_ficha=ficha::onGet($nacionalidad,$cedula);
 $id_ficha=$detalle_ficha[0]["id"];
 
-
 $organismo=$db->Execute("SELECT P.identificacion_tipo||P.identificacion_numero as identificacion
                           FROM
                             modulo_base.organismo AS O,
                             modulo_base.persona as P
                           WHERE
                             O.id_persona=P.id");
-
-//buscar el ultimo periodo cerrado en la nómina
-/*$periodo=$db->Execute("SELECT id, fecha_culminacion
-                       FROM modulo_nomina.periodo
-                       WHERE tipo='Q' and cerrado AND EXTRACT(MONTH FROM fecha_inicio)=EXTRACT(MONTH FROM fecha_culminacion)
-                       ORDER BY fecha_culminacion desc, codigo asc
-                       LIMIT 1");*/
 
 //buscar el ultimo periodo cerrado donde estuvo la persona
 $sql="
@@ -95,18 +71,13 @@ $sql="
 ";
 //print $sql;
 $periodo=$db->Execute($sql);
-
-
-
-
-
 if(!isset($periodo[0][0])){
     print "No encontro el ultimo periodo.";
     exit;
 }
 $id_periodo=$periodo[0]["id"];
-
 $fecha_culminacion=$periodo[0]["fecha_culminacion"];
+
 
 
 
@@ -134,14 +105,6 @@ $sql="select
       where
         FC.id_ficha=$id_ficha AND
         FC.id_periodo=$id_periodo";
-$sql="select
-        distinct id_nomina
-      from
-        modulo_nomina.ficha_concepto as FC
-      where
-        FC.id_ficha=$id_ficha AND
-        FC.id_periodo=$id_periodo";
-        //print $sql;
 $nomina=$db->Execute($sql);    
 
 if(!isset($nomina) or count($nomina)==0){
@@ -155,23 +118,97 @@ $cargo=$cargo[0]["cargo_denominacion"];
 
 
 $sueldo_normal=0;
+$primas=0;
 $diferencia_sueldo=0;
-for($i=0;$i<count($nomina);$i++){
+$prima_responsabilidad=0;
+$prima_antiguedad=0;
+$prima_prof=0;
+$prima_hijos=0;
+
+for($i=0;$i<count($nomina);$i++)
+{
   $concepto=nomina::ficha_concepto($nomina[$i]["id_nomina"],$id_periodo,$id_ficha);
   $concepto=$concepto["concepto"];
-  for($k=0;$k<count($concepto);$k++){
-    if($concepto[$k]["identificador"]=="SUELDO_NORMAL")
+  for($k=0;$k<count($concepto);$k++)
+  {
+    if(in_array($concepto[$k]["identificador"],["SUELDO_MENSUAL"]))//if($concepto[$k]["identificador"]=="SUELDO_BASICO")
       $sueldo_normal+=$concepto[$k]["valor_final"];
-    else if(substr_compare($concepto[$k]["identificador"],"DIFERENCIA_",0,11)==0)
+    //else if(in_array($concepto[$k]["identificador"],["PRIMA_PROF","PRIMA_HIJOS"]))
+    //  $primas+=$concepto[$k]["valor_final"];
+    else if($concepto[$k]["identificador"]=="DIFERENCIA_SUELDO")
       $diferencia_sueldo+=$concepto[$k]["valor_final"];
+
+    if($concepto[$k]["identificador"]=="PRIMA_RESPONSABILIDAD")
+      $prima_responsabilidad+=$concepto[$k]["valor_final"];
+
+    if($concepto[$k]["identificador"]=="PRIMA_ANTIGUEDAD")
+      $prima_antiguedad+=$concepto[$k]["valor_final"];
+
+    if($concepto[$k]["identificador"]=="PRIMA_PROF")
+      $prima_prof+=$concepto[$k]["valor_final"];
+
+    if($concepto[$k]["identificador"]=="PRIMA_HIJOS")
+      $prima_hijos+=$concepto[$k]["valor_final"];
   }
 }
 
+$sueldo_normal=($sueldo_normal-$diferencia_sueldo);
+/*suma de primas.*/
 
 
-//print $diferencia_sueldo;exit;
-//$sueldo_normal=($sueldo_normal-$diferencia_sueldo)*2;
-$sueldo_normal=($sueldo_normal-$diferencia_sueldo)*2;
+if(empty($prima_antiguedad) & empty($prima_responsabilidad) & empty($prima_prof) & empty($prima_hijos))
+{
+  $id_periodo=$id_periodo-1;
+
+  $sql="select
+        distinct id_nomina
+      from
+        modulo_nomina.ficha_concepto as FC
+      where
+        FC.id_ficha=$id_ficha AND
+        FC.id_periodo=$id_periodo";
+  $nomina=$db->Execute($sql);  
+
+  for($i=0;$i<count($nomina);$i++)
+  {
+    $concepto=nomina::ficha_concepto($nomina[$i]["id_nomina"],$id_periodo,$id_ficha);
+    $concepto=$concepto["concepto"];
+    for($k=0;$k<count($concepto);$k++)
+    {
+      if($concepto[$k]["identificador"]=="PRIMA_RESPONSABILIDAD")
+        $prima_responsabilidad+=$concepto[$k]["valor_final"];
+
+      if($concepto[$k]["identificador"]=="PRIMA_ANTIGUEDAD")
+        $prima_antiguedad+=$concepto[$k]["valor_final"];
+
+       if($concepto[$k]["identificador"]=="PRIMA_PROF")
+        $prima_prof+=$concepto[$k]["valor_final"];
+
+      if($concepto[$k]["identificador"]=="PRIMA_HIJOS")
+        $prima_hijos+=$concepto[$k]["valor_final"];
+    }
+  }
+}
+
+if(!empty($prima_antiguedad))
+{
+  $sueldo_normal=$sueldo_normal+$prima_antiguedad;
+}
+
+if(!empty($prima_responsabilidad))
+{
+  $sueldo_normal=$sueldo_normal+$prima_responsabilidad;
+}
+
+if(!empty($prima_prof))
+{
+  $sueldo_normal=$sueldo_normal+$prima_prof;
+}
+
+if(!empty($prima_hijos))
+{
+  $sueldo_normal=$sueldo_normal+$prima_hijos;
+}
 
 $formato="A";
 $formato="B";//con antiguedad
@@ -191,11 +228,11 @@ $pdf->Image(SIGA::databasePath()."/config/plantilla_vertical.jpg",0,0,215);
 $pdf->SetFont('helvetica','B',12);
 $rif_len=strlen($organismo[0]["identificacion"])-1;
 $rif=$organismo[0]["identificacion"][0]."-".substr($organismo[0]["identificacion"],1,$rif_len-1)."-".$organismo[0]["identificacion"][$rif_len];
-$pdf->Text(25,28,utf8_decode("RIF: $rif"));
+//$pdf->Text(25,35,utf8_decode("RIF: $rif"));
 
-$pdf->SetFont('helvetica','BI',24);
+$pdf->SetFont('helvetica','BI',20);
 $pdf->SetXY(9,40);
-$pdf->MultiCell(196,6,utf8_decode("C O N S T A N C I A"),'','C');
+$pdf->MultiCell(196,6,utf8_decode("C O N S T A N C I A   D E   T R A B A J O"),'','C');
 
 $pdf->SetStyle("parrafo", "helvetica", "I", 13, "0, 0, 0", 15);
 $pdf->SetStyle("negrita", "helvetica", "BI", 13, "0, 0, 0");
@@ -209,21 +246,27 @@ if(isset($detalle_ficha[0]["genero"])){
     elseif($detalle_ficha[0]["genero"]=="F") $persona="la ciudadana";
 }
 
+$ingreso="";
+
+if(isset($detalle_ficha[0]["fecha_ingreso"])){
+  $ingreso=$detalle_ficha[0]["fecha_ingreso"];
+  $ingreso=$detalle_ficha[0]["fecha_ingreso"][0];
+  $ingreso=formatDate($ingreso);
+}
+
 switch($formato){
     case "B":
         $antiguedad="";
         if(isset($detalle_ficha[0]["antiguedad_simple"]) and $detalle_ficha[0]["antiguedad_simple"])
             $antiguedad=$detalle_ficha[0]["antiguedad_simple"];
-        //$antiguedad="5 años y 264 días";
-        $texto= "Quien suscribe Soc. Enrique José Ortiz Rodríguez, titular de la cédula de identidad ".
-                "Nº 8.647.822, Presidente de la Fundación para el Desarrollo de la Ciencia y Tecnología en el Estado Sucre (FUNDACITE SUCRE), ".
+        $texto= "Quien suscribe, Lcdo. Carlos Julio Velasquez, Director de Personal de la Alcadía Bolivariana del Municipio Mejía del Estado Sucre, ".
                 "por medio de la presente hace constar que $persona: <negrita>$nombres_apellidos,</negrita> titular de la cédula de identidad <negrita>Nº ".$nacionalidad."-".number_format($cedula,0,"",".").",</negrita> ".
-                "ha prestado servicio en la Institución durante $antiguedad. Actualmente desempeña el cargo de $cargo y devenga una remuneración mensual de ".letra_numero($sueldo_normal,true)." Céntimos (Bs ".number_format($sueldo_normal,2,",",".").").";
+                "ingreso a la institución desempeñando el cargo de $cargo desde el $ingreso, devengando un sueldo mensual de ".letra_numero($sueldo_normal,true)." Céntimos (Bs ".number_format($sueldo_normal,2,",",".").").";
     break;
     case "A":
     default:
-        $texto= "Quien suscribe Soc. Enrique José Ortiz Rodríguez, titular de la cédula de identidad ".
-                "Nº 8.647.822, Presidente de la Fundación para el Desarrollo de la Ciencia y Tecnología en el Estado Sucre (FUNDACITE SUCRE), ".
+        $texto= "Quien suscribe Lcdo. Carlos Julio Velasquez, titular de la cédula de identidad ".
+                "Nº 18.581.526, Director de Personal de la Alcadía Bolivariana del Municipio Mejía, ".
                 "por medio de la presente hace constar que la persona: <negrita>$nombres_apellidos</negrita>, titular de la cédula de identidad <negrita>Nº ".$nacionalidad."-".number_format($cedula,0,"",".")."</negrita>, ".
                 "labora en esta institución desempeñando el cargo de $cargo y devenga una remuneración mensual de ".letra_numero($sueldo_normal,true)." Céntimos (Bs ".number_format($sueldo_normal,2,",",".").").";
 }
@@ -231,32 +274,32 @@ switch($formato){
 
 $pdf->WriteTag(170,9,utf8_decode(
     "<parrafo>$texto</parrafo>".
-    "<parrafo>Se expide la presente constancia a petición de parte interesada, en Cumaná, a los ".letra_numero(date("d"))." días del mes de ".letra_mes(date("m"))." de ".date("Y").".</parrafo>"),'','J');
+    "<parrafo>Se expide la presente constancia a petición de parte interesada, en San Antonio del Golfo, a los ".letra_numero(date("d"))." días del mes de ".letra_mes(date("m"))." de ".date("Y").".</parrafo>"),'','J');
 
 
 
 $pdf->SetFont('helvetica','BI',13);
-$pdf->SetXY(22,220);
-$pdf->MultiCell(170,6,utf8_decode("Soc. Enrique José Ortiz Rodríguez"),'','C');
-
-$pdf->SetFont('helvetica','',13);
-$pdf->SetXY(22,220+6);
-$pdf->MultiCell(170,6,utf8_decode("Presidente de la FUNDACITE Sucre"),'','C');
+$pdf->SetXY(22,200);
+$pdf->MultiCell(170,6,utf8_decode("Lcdo. Carlos Julio Velasquez"),'','C');
 
 $pdf->SetFont('helvetica','',12);
-$pdf->SetXY(22,220+6*2);
-$pdf->MultiCell(170,5,utf8_decode("Resolución Nº 211 de fecha 18/11/2013 publicado en la Gaceta Oficial de la República Bolivariana de Venezuela Nº 40.297 de fecha 19/11/2013"),'','C');
+$pdf->SetXY(22,200+6);
+$pdf->MultiCell(170,6,utf8_decode("C.I: 18.581.526\nDirector de Personal\nSegún Resolución 019/2025\nDe fecha 28/08/2025"),'','C');
+
+$pdf->SetFont('helvetica','',10);
+$pdf->SetXY(22,224+6*2);
+$pdf->MultiCell(170,5,utf8_decode("Calle Santa Teresa - Frente a la Plaza Bolivar - San Antonio del Golfo, Municipio Mejía del Estado Sucre. Telf: (0293)829.32.40 / Correo: alcaldiamejiarh13@gmail.com / Rif: $rif\n"),'','C');
 
 $pdf->SetFont('helvetica','I',9);
 $pdf->SetXY(22,260);
-$pdf->MultiCell(170,4,utf8_decode("Av. Monseñor Alfredo Rodríguez Figueroa, vía El Peñon, Urb. Cristobal Colón, Cumaná 6101, Edo. Sucre, Venezuela.\nTelf. (0293)441.20.63 - http://www.fundacite-sucre.gob.ve/"),'','C');
+//$pdf->MultiCell(170,4,utf8_decode("Av. Monseñor Alfredo Rodríguez Figueroa, vía El Peñon, Urb. Cristobal Colón, Cumaná 6101, Edo. Sucre, Venezuela.\nTelf. (0293)467.25.31 - http://www.fundacite-sucre.gob.ve/"),'','C');
 
 
 
 
 $pdf->SetDrawColor(160,0,0);
-$pdf->Line(15, 30, 15, 260-5);
-$pdf->Line(15+3, 30+3, 15+3, 260+3-5);
+$pdf->Line(15, 10+30, 15, 260-5);
+$pdf->Line(15+3, 10+30+3, 15+3, 260+3-5);
 
 $pdf->Line(15, 260-5, 195, 260-5);
 $pdf->Line(15+3, 260+3-5, 195+3, 260+3-5);
